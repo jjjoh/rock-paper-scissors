0d8d4c2cb2a992efa84f7c5c8f5db7f3d7d6fdd8
johanne <johanne@mrsuricate.com>
2025-01-14 15:13:24 +0100

    feat single round play

---
+++ index/main.js
@@ -0,0 +1,51 @@
+
+
+let computerChoice = "not decided yet";
+let humanChoice = "not decided yet";
+
+let humanScore = 0;
+let computerScore = 0;
+
+function getComputerChoice() {
+    const x = Math.random();
+    if ( x < 0.334 ) { computerChoice = "rock"}
+    else if ( (0.334 <= x) && (x <= 0.68) ) { computerChoice = "paper"}
+    else  { computerChoice = "scissors"}
+    return computerChoice
+}
+
+
+function getHumanChoice() {
+    humanChoice = prompt("Paper, rock, scissors, what will be your choice ? To choose paper enter 1, to choose rock enter 2, to choose scissors enter 3", "Enter your choice here" )
+    if ( humanChoice == "1") { humanChoice = "paper"}
+    else if (humanChoice == "2" ) { humanChoice = "rock"}
+    else  if (humanChoice == "3" ) { humanChoice = "scissors"}
+    return humanChoice
+
+}
+
+
+function playRound(humanChoice, computerChoice) {
+    let a = humanChoice + ' ' + computerChoice;
+    switch (a) {
+        case 'paper rock':
+        case 'scissors paper':
+        case 'rock scissors':
+            console.log(a,': You win');
+            break;
+
+        case 'paper scissors':
+        case 'scissors rock':
+        case 'rock paper':
+            console.log(a,': You loose');
+            break;
+
+        default:
+            console.log(a,': No ones wins');
+    }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice()
+
+playRound(humanSelection, computerSelection);
